82288189e38b08b111981f3395b8a584c7691b79
watermelon91 <yjtang@umich.edu>
2014-11-15 15:57:32 -0500

    controlled group UI

--- SimpleQuery/src/mainWindow/UserWindow.java
+++ SimpleQuery/src/mainWindow/UserWindow.java
@@ -6,25 +6,50 @@ package mainWindow;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.util.Arrays;
 
 import javax.swing.*;
 
 import com.jgoodies.forms.factories.*;
 import com.jgoodies.forms.layout.*;
 
+import frontEndConnector.FrontEndConnector;
+import frontEndConnector.FrontEndConnector.Pair;
+
 /**
  * @author abc def
  */
 public class UserWindow extends JFrame {
 	
+	private FrontEndConnector connector = new FrontEndConnector("yijiadanajie.cta5xgwtrfyv.us-west-2.rds.amazonaws.com", "mydb", "yijia", "eecs58414");
+	
 	public UserWindow() {
 		super();
 		initComponents();
+		connector.initializeSQLConnection();
 	}
 
 	private void submitButtonMouseClicked(MouseEvent e) {
 		// TODO add your code here
 		System.out.println("Submit clicked");
+		
+		String query = queryEntertextPanel.getText();;
+		Pair result = connector.executeQuerySeparateResult(query, Integer.MAX_VALUE);
+		
+		for(int i = 0; i < result.data.size(); i++)
+		{
+			System.out.println(Arrays.asList(result.data.get(i)));
+		}
+	}
+
+	private void windowWindowClosing(WindowEvent e) {
+		// TODO add your code here
+		connector.closeDBConnection();
+	}
+
+	private void queryEntertextPanelKeyTyped(KeyEvent e) {
+		// TODO add your code here
+		
 	}
 
 
@@ -35,7 +60,7 @@ public class UserWindow extends JFrame {
 		
 		// JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
 		// Generated using JFormDesigner Evaluation license - abc def
-		frame1 = new JFrame();
+		window = new JFrame();
 		queryEnterLabel = new JLabel();
 		resultDisplayLabel = new JLabel();
 		scrollPane1 = new JScrollPane();
@@ -44,54 +69,68 @@ public class UserWindow extends JFrame {
 		resultTable = new JTable();
 		submitButton = new JButton();
 
-		//======== frame1 ========
+		//======== window ========
 		{
-			Container frame1ContentPane = frame1.getContentPane();
-			frame1ContentPane.setLayout(new FormLayout(
+			window.addWindowListener(new WindowAdapter() {
+				@Override
+				public void windowClosing(WindowEvent e) {
+					windowWindowClosing(e);
+				}
+			});
+			Container windowContentPane = window.getContentPane();
+			windowContentPane.setLayout(new FormLayout(
 				"default, $lcgap, 150dlu, $lcgap, 284dlu",
 				"default, $lgap, 94dlu, $lgap, 18dlu, $lgap, 169dlu"));
 
 			//---- queryEnterLabel ----
-			queryEnterLabel.setText("text");
-			frame1ContentPane.add(queryEnterLabel, CC.xy(3, 1));
+			queryEnterLabel.setText("Query to send");
+			windowContentPane.add(queryEnterLabel, CC.xy(3, 1));
 
 			//---- resultDisplayLabel ----
-			resultDisplayLabel.setText("text");
-			frame1ContentPane.add(resultDisplayLabel, CC.xy(5, 1));
+			resultDisplayLabel.setText("Query result");
+			windowContentPane.add(resultDisplayLabel, CC.xy(5, 1));
 
 			//======== scrollPane1 ========
 			{
+
+				//---- queryEntertextPanel ----
+				queryEntertextPanel.addKeyListener(new KeyAdapter() {
+					@Override
+					public void keyTyped(KeyEvent e) {
+						queryEntertextPanelKeyTyped(e);
+					}
+				});
 				scrollPane1.setViewportView(queryEntertextPanel);
 			}
-			frame1ContentPane.add(scrollPane1, CC.xywh(3, 2, 1, 2));
+			windowContentPane.add(scrollPane1, CC.xywh(3, 2, 1, 2));
 
 			//======== scrollPane2 ========
 			{
 				scrollPane2.setViewportView(resultTable);
 			}
-			frame1ContentPane.add(scrollPane2, CC.xywh(5, 2, 1, 6));
+			windowContentPane.add(scrollPane2, CC.xywh(5, 2, 1, 6));
 
 			//---- submitButton ----
-			submitButton.setText("text");
+			submitButton.setText("Submit");
 			submitButton.addMouseListener(new MouseAdapter() {
 				@Override
 				public void mouseClicked(MouseEvent e) {
 					submitButtonMouseClicked(e);
 				}
 			});
-			frame1ContentPane.add(submitButton, CC.xy(3, 5));
-			frame1.pack();
-			frame1.setLocationRelativeTo(frame1.getOwner());
+			windowContentPane.add(submitButton, CC.xy(3, 5));
+			window.pack();
+			window.setLocationRelativeTo(window.getOwner());
 			
-			setBounds(frame1ContentPane.getBounds());
-			add(frame1ContentPane);
+			setSize(windowContentPane.getSize());
+			add(windowContentPane);
 		}
 		// JFormDesigner - End of component initialization  //GEN-END:initComponents
 	}
 
 	// JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
 	// Generated using JFormDesigner Evaluation license - abc def
-	private JFrame frame1;
+	private JFrame window;
 	private JLabel queryEnterLabel;
 	private JLabel resultDisplayLabel;
 	private JScrollPane scrollPane1;
