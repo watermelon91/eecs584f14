43d7bd59c35d5b981c61e71839ed11c60d9753cf
watermelon91 <yjtang@umich.edu>
2014-11-15 15:54:21 -0500

    add case insensitive

--- QueryParserTest/src/databaseConnector/PostgresDBConnector.java
+++ QueryParserTest/src/databaseConnector/PostgresDBConnector.java
@@ -110,6 +110,8 @@ public class PostgresDBConnector {
 	
 	public List<String[]> executeQuerySeparateResult(String query, int LIMIT, String tableName) throws SQLException, InputQueryNotSELECTALL, QueryAttrNumNotMatch
 	{
+		query = query.toUpperCase();
+		
 		// the input query has to start with "SELECT * FROM", otherwise difficulty to track attr types
 		if(!query.startsWith("SELECT * FROM"))
 		{
