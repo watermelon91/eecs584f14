1d6691892ea892a8180125844c20052e40726198
watermelon91 <yjtang@umich.edu>
2014-11-15 23:12:06 -0500

    controlled group UI

--- SimpleQuery/src/mainWindow/LoggingUtilities.java
+++ SimpleQuery/src/mainWindow/LoggingUtilities.java
@@ -70,7 +70,7 @@ public class LoggingUtilities {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		sendFromGMail(sender, pwd, new String[] {receipt}, "584log", logs);
+		sendFromGMail(sender, pwd, new String[] {receipt}, "584log_controlledGroup", logs);
 		
 		System.out.println("\nlog sent");
 	}
